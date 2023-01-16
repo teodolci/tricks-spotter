69d00acafaf704b8a771743338a37201ac4ed21e
David Wismer <david.wismer@heig-vd.ch>
2023-01-16 13:17:33 +0100

    Solving bug with signup routing

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -15,16 +15,6 @@ const routes: Routes = [
         pathMatch: "full",
       },
       {
-        // Route pour le login
-        path: 'login',
-        loadChildren: () => import('../auth/login/login.module').then(m => m.LoginPageModule)
-      },
-      {
-        // Route pour le signup
-        path: 'signup',
-        loadChildren: () => import('../auth/signup/signup.module').then(m => m.SignupPageModule)
-      },
-      {
         // Route pour la page d'accueil
         path: 'accueil',
         loadChildren: () => import('./accueil/accueil.module').then(m => m.AccueilPageModule)
