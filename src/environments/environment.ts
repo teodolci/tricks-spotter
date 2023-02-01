e8d9f796b77fb7c144b11f799fc5b68f7a5995be
Anas Ismaili <anas.ismaili@heig-vd.ch>
2023-02-01 17:36:39 +0100

    added env

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+// Copy this file to environment.ts and replace the values with your configuration
+export const environment = {
+    production: false,
+    apiUrl: "https://tricks-spotter-api.onrender.com",
+    qimgUrl: "https://qimg.onrender.com/api",
+    qimgSecret: "<change-with-your-token>", //Forgot to ask for the token
+  };
